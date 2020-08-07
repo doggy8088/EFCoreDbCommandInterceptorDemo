c99ecd5ec833c3982eaaa080d8f6ed88a3c95972
Will <doggy.huang@gmail.com>
2020-08-08 00:56:12 +0800

    加入 EF Core 基本範例程式

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using efdemo1.Models;
 
 namespace efdemo1
 {
@@ -6,7 +7,61 @@ namespace efdemo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new TodoContext())
+            {
+                db.Todos.Add(new Todo() {
+                    Item = "Item 1"
+                });
+
+                db.SaveChanges();
+            }
+
+            using (var db = new TodoContext())
+            {
+                Console.WriteLine("查詢新增後的所有資料");
+                foreach (var item in db.Todos)
+                {
+                    Console.WriteLine(item.Id + " " + item.Item);
+                }
+            }
+
+            using (var db = new TodoContext())
+            {
+                foreach (var item in db.Todos)
+                {
+                    item.Item += "!";
+                }
+
+                db.SaveChanges();
+            }
+
+            using (var db = new TodoContext())
+            {
+                Console.WriteLine("查詢更新後的所有資料");
+                foreach (var item in db.Todos)
+                {
+                    Console.WriteLine(item.Id + " " + item.Item);
+                }
+            }
+
+            using (var db = new TodoContext())
+            {
+                var item = db.Todos.Find(1);
+                if (item != null)
+                {
+                    db.Todos.Remove(item);
+                    db.SaveChanges();
+                }
+            }
+
+            using (var db = new TodoContext())
+            {
+                Console.WriteLine("查詢刪除編號 1 資料後的所有資料");
+                foreach (var item in db.Todos)
+                {
+                    Console.WriteLine(item.Id + " " + item.Item);
+                }
+            }
         }
     }
 }
