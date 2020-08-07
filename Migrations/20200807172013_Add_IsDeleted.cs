33a60ddea5a1938a5bb82211f4215855e936c12b
Will <doggy.huang@gmail.com>
2020-08-08 01:35:30 +0800

    實驗有兩個以上的資料庫移轉版本會導致 ALTER DATABASE CURRENT COLLATE Chinese_Taiwan_Stroke_CI_AS 命令執行完畢後會將連線中斷

---
+++ Migrations/20200807172013_Add_IsDeleted.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace efdemo1.Migrations
+{
+    public partial class Add_IsDeleted : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Todos",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Todos");
+        }
+    }
+}
--- Models/Todo.cs
+++ Models/Todo.cs
@@ -2,4 +2,6 @@ public class Todo
 {
     public int Id { get; set; }
     public string Item { get; set; }
+    // 新增一個 IsDeleted 屬性
+    public bool IsDeleted { get; set; }
 }
