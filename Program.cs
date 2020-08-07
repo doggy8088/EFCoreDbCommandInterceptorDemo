65b48082da1ef5a01c469d14e835740d9b0cb7fa
Will <doggy.huang@gmail.com>
2020-08-08 00:17:56 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace efdemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
